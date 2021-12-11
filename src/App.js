00a34abe2e63609cb7329cc6ce2533f13430af4a
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-12-10 20:04:39 -0600

    styled nav bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import "../src/styles.css"
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function NavBar(){
     return (
-      
-           <h3>NavBar goes here</h3>
+      <div className="nav">
+
+        <img height="50px" width="50px" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" />
+           <h3>React Facts</h3>
     
+      </div>
     );
 }
 
