fecd33ead10bd98f4ff1643f83f4604a3d993590
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-12-12 19:10:29 -0600

    added facts

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,22 @@ import React from 'react';
 
 function Main(){
     return (
-      
-           <h1>Main section goes here</h1>
+        <div>
+
+           <h1>Facts about React!</h1>
+
+                <ul>
+                    <li>Intial release date was 2013</li>
+                    <li>Jordan Walke is the original creator</li>
+                    <li>Currently has over 1,500 contributors on github </li>
+                    <li>Is maintained by Facebook</li>
+
+
+
+                </ul>
+
+
+        </div>
     );
 }
 
